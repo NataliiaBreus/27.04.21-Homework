515cf0530303c94327928a0ad1d7ba7c7f8a686d
NataliiaBreus <breusnataly@gmail.com>
2021-04-28 08:01:37 +0300

    placeCard tasks

---
+++ assets/js/index.js
@@ -0,0 +1,183 @@
+'use strict';
+// Refactor createPlaceCard (второй вариант от 26.04.21)
+const cardContainer = document.getElementById("root");
+
+const cardElements = data.map((place) => createPlaceCard(place));
+cardContainer.append(...cardElements);
+
+function createPlaceCard(place) {
+  return createElement(
+    "li",
+    { classNames: ["cardWrapper"] },
+    // article.cardContainer
+    createElement(
+      "article",
+      { classNames: ["cardContainer"] },
+      createImageWrapper(place),
+      createContentWrapper(place)
+    )
+  );
+}
+
+function createCardImage(link) {
+  const img = createElement("img", {
+    classNames: ["cardImage"],
+    handlers: {
+      error: handleImageError,
+      load: handleImageLoad,
+    },
+  });
+  img.src = link;
+  img.hidden = true;
+
+  return img;
+}
+
+function createImageWrapper({ name, profilePicture }) {
+  // div.cardImageWrapper
+  const imageWrapper = createElement(
+    "div",
+    {
+      classNames: ["cardImageWrapper"],
+    },
+    // div.initials
+    createElement(
+      "div",
+      { classNames: ["initials"] },
+      document.createTextNode(name[0] || "")
+    ),
+    createCardImage(profilePicture)
+  );
+  imageWrapper.style.backgroundColor = stringToColor(name || "");
+
+  return imageWrapper;
+}
+
+function createContentWrapper({ name, description }) {
+  return createElement(
+    "div",
+    {
+      classNames: ["contentWrapper"],
+    },
+    // h3.cardName
+    createElement(
+      "h3",
+      { classNames: ["cardName"] },
+      document.createTextNode(name || "")
+    ),
+    // p.cardDescription
+    createElement(
+      "p",
+      { classNames: ["cardDescription"] },
+      document.createTextNode(description || "")
+    )
+  );
+}
+
+/**
+ *
+ * @param {string} tagName
+ * @param {object} options
+ * @param {string[]} options.classNames - css classes
+ * @param {object} options.handlers - event handlers
+ * @param  {...Node} children
+ * @returns {HTMLElement}
+ */
+function createElement(
+  tagName,
+  { classNames = [], handlers = {} },
+  ...children
+) {
+  const elem = document.createElement(tagName);
+  elem.classList.add(...classNames);
+
+  for (const [eventType, eventHandler] of Object.entries(handlers)) {
+    elem.addEventListener(eventType, eventHandler);
+  }
+
+  elem.append(...children);
+  return elem;
+}
+
+/*
+  EVENT HANDLERS
+*/
+
+function handleImageError({ target }) {
+  target.remove();
+}
+
+function handleImageLoad({ target }) {
+  target.hidden = false;
+}
+
+/*
+  UTILS
+*/
+
+function stringToColor(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = "#";
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ("00" + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
+
+
+
+
+
+
+
+const form = document.getElementById("root-form");
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    const {target: {elements:{email}}} = event;
+    console.dir(event.target);
+});
+
+// task 1-2
+const form = document.getElementById("root-form");
+const list = document.getElementById('root-list');
+
+
+form.addEventListener("submit", submitHandler);
+
+const arr = [];
+
+function submitHandler (event) {
+  event.preventDefault();
+  const {
+    target,
+    target: {
+      elements: {test2 },
+    },
+  } = event;
+
+  arr.push(test2.value);
+  list.append(createListItem(test2.value));
+
+  target.reset();
+}
+
+// Task 3
+function createListItem(value) {
+    const listItem = document.createElement('li');
+    listItem.innerText = value;
+
+    return listItem;
+}
+
+
+
+
+
+
+
---
+++ assets/js/sandbox.js
@@ -0,0 +1,64 @@
+// Тема замыкание
+/*
+function makeCounter(){
+    let count = 0;
+    function counter (){
+        
+        return ++ count;
+    }
+}
+const counter1 = makeCounter();
+const counter2 = makeCounter();
+
+
+// task 1
+
+function createAdder(n) {
+    return function (m) {
+        return n +=m;
+    }
+};
+     const adder = createAdder(10);
+
+
+
+const btn = document.querySelector('#unique');
+
+function createBtnHandler(clicksAmount=5) {
+    const btnHandler = ({target: targetBtn}) =>{
+        if(clickAmount <= 0) {
+            targetBtn.removeEventListener("click, btnHandler");
+            return;
+        }
+        alert ('success');
+        clicksAmount --; 
+    };
+    return btnHandler;
+}
+
+btn.addEventListener('click', createBtnHandler(3));
+btn2.addEventListener('click', createBtnHandler(2));
+
+
+btn.addEventListener("click", (e)=>{
+    console.group();
+    console.log(e);
+    console.log(e.target === btn);
+    console.log(e.currentTarget);
+    console.groupEnd();
+});
+btn.addEventListener("click", btnHandler);
+
+window.addEventListener("click", btnHandler);
+document.addEventListener("click", btnHandler);
+document.body.addEventListener("click", btnHandler);
+*/
+
+
+
+
+
+
+         
+
+        
